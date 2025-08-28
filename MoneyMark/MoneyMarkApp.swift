48062cadefa924e38859e360eeb4cc97c15cc899
Austin <wardaustin2001@gmail.com>
2025-08-28 17:18:18 -0400

    Initial Commit

---
+++ MoneyMark/MoneyMarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyMarkApp.swift
+//  MoneyMark
+//
+//  Created by Austin on 8/28/25.
+//
+
+import SwiftUI
+
+@main
+struct MoneyMarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
